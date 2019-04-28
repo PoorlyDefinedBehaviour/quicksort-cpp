2100b260d973ae4786f5d8f053333ae160c85e46
Bruno <brunotj2015@hotmail.com>
2019-04-28 18:06:45 -0300

    perfect forwarding the array

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,46 @@
 #include <iostream>
-#include <algorithm>
-#include <vector>
 #include <array>
+#include <vector>
 
 template <typename T>
-void quicksort(T &array, const int &start, const int &end)
+void quicksort(T &&array, const int &start, const int &end)
 {
   if (start >= end)
     return;
 
-  auto partition = [&]() -> int {
-    int pivotIndex = start;
-    auto pivotValue = array[end];
-
-    for (int i = start; i < end; ++i)
-    {
-      if (array[i] < pivotValue)
-      {
-        std::swap(array[i], array[pivotIndex]);
-        ++pivotIndex;
-      }
-    }
-    std::swap(array[pivotIndex], array[end]);
-    return pivotIndex;
+  auto swap = [](auto &a, auto &b) -> void {
+    auto temp = a;
+    a = b;
+    b = temp;
   };
 
-  int currentIndex = partition();
-  quicksort(array, start, currentIndex - 1);
-  quicksort(array, currentIndex + 1, end);
+  int pivotIndex = start;
+  auto pivotValue = array[end];
+
+  for (int i = start; i < end; ++i)
+  {
+    if (array[i] < pivotValue)
+    {
+      swap(array[i], array[pivotIndex]);
+      ++pivotIndex;
+    }
+  }
+
+  swap(array[pivotIndex], array[end]);
+
+  quicksort(std::forward<T>(array), start, pivotIndex - 1);
+  quicksort(std::forward<T>(array), pivotIndex + 1, end);
 }
 
 int main()
 {
-  std::vector<int> std_vector = {5, 2, 8, 7, 2, 9, 10, 18, 29, 3};
-  std::array<int, 10> std_array = {5, 2, 8, 7, 2, 9, 10, 18, 29, 3};
-  int array[] = {5, 2, 8, 7, 2, 9, 10, 18, 29, 3};
+  std::vector<int> std_vector = {6, 2, 7, 1, 0, 29, 48, 2, 49, 3};
+  std::array<int, 10> std_array = {6, 2, 7, 1, 0, 29, 48, 2, 49, 3};
+  int c_array[] = {6, 2, 7, 1, 0, 29, 48, 2, 49, 3};
 
   quicksort(std_vector, 0, std_vector.size() - 1);
   quicksort(std_array, 0, std_array.size() - 1);
-  quicksort(array, 0, 9);
+  quicksort(c_array, 0, 9);
 
   std::cout << "\nstd::vector -> ";
   for (const auto &element : std_vector)
@@ -48,7 +50,7 @@ int main()
   for (const auto &element : std_array)
     std::cout << element << " ";
 
-  std::cout << "\narray -> ";
-  for (const auto &element : array)
+  std::cout << "\nc style array -> ";
+  for (const auto &element : c_array)
     std::cout << element << " ";
 }
